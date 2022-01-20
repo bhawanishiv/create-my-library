9be8b4f7775fe1285f1e4c4e9e7695cd94f9182b
Bhawani Shankar Bharti <bhawanishiv@gmail.com>
2022-01-21 03:04:18 +0530

    Add git initialization

--- src/cli.js
+++ src/cli.js
@@ -5,12 +5,9 @@ const { promptOptions } = require("./prompts");
 
 exports.cli = async (args) => {
   try {
-    console.log(`args->`, args);
     const options = getProgramOptions(args);
-    console.log(`options->`, options);
     const values = await promptOptions(options);
-    console.log(`values->`, values);
-    await prepareLibraryFiles(values);
+    await prepareLibraryFiles({ ...options, ...values });
   } catch (e) {
     console.error("%s: %s", chalk.red.bold("ERROR"), e.message);
     process.exit(1);
